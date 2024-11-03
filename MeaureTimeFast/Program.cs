a4e40dc96bfc3d4421a5a306282d7e65281bfa23
paul-green <greenpj@yahoo.com>
2024-11-04 08:56:42 +1100

    MeasureTime

---
+++ MeaureTimeFast/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+const int durSecs = 1;
+
+void UsingUtcNow()
+{
+    var then = DateTime.UtcNow.AddSeconds(durSecs);
+    var count = 0;
+    while (DateTime.UtcNow < then) count++;
+    Console.WriteLine($"DateTime.UtcNow count in : {count:N}");
+}
+
+void UsingStopwatchElapsed()
+{
+    var then = TimeSpan.FromSeconds(durSecs);
+    var count = 0;
+    Stopwatch.GetTimestamp()
+    var sw = Stopwatch.StartNew();
+    while (sw.Elapsed < then) count++;
+    Console.WriteLine($"Stopwatch.Elapsed count in : {count:N}");
+}
+
+UsingUtcNow();
+UsingStopwatchElapsed();
---
+++ MeaureTimeFast/readme.md
@@ -0,0 +1,11 @@
+﻿In .Net 9 the Stopwatch has improved performance.
+
+Prior to .9 it would use DateTime.UtcNow, which had a good amount of extra code to check for timezones/leap-seconds etc, which reduced its 
+performance.
+
+```Stopwatch.StartNew()``` now uses ``` Stopwatch.GetTimestamp()``` which uses the O/S kernal hardware timer - which is usually the case. If not it 
+will fallback to ```DateTime.UtcNow```
+
+``` Stopwatch.GetTimestamp()``` is hardware specific and can't be assumed to be consistent across devices - so don't perform calculations assuming
+it's ticks for example.
+
